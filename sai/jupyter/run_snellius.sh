c3c3ea4022f2133533861ff25fe24fa9d081f285
Jasper Jong <jasperdj@int6.local.snellius.surf.nl>
2023-05-09 17:36:10 +0200

    debugged jupyter launching script

--- sai/jupyter/run_snellius.sh
+++ sai/jupyter/run_snellius.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
+
 #SBATCH -t 00:05:00          # wall clock time
 #SBATCH -N 1                 # number of nodes
-#SBATCH -p normal            # partition
+#SBATCH -p thin              # partition
 #SBATCH -J testrun           # job name
 #SBATCH -A uuce20038         # account
 #SBATCH -n 1                 # number of tasks
 #SBATCH --cpus-per-task=32   # cpus per task
 #SBATCH --mem=56G            # memory per node limit
-#SBATCH -e PWD               # file receiving standard error
-#SBATCH -o PWD               # file receiving standard output
 
 echo "Running ${0} ..."
 echo "Submitted from:         ${SLURM_SUBMIT_HOST}:${SLURM_SUBMIT_DIR}"
@@ -36,7 +35,7 @@ conda env list
  
 # Choose random port and print instructions to connect
 PORT=`shuf -i 5000-5999 -n 1`
-LOGIN_HOST=int4-pub.snellius.surf.nl
+LOGIN_HOST=int6-pub.snellius.surf.nl
 BATCH_HOST=$(hostname)
  
 echo "To connect to the notebook type the following command from your local terminal:"
@@ -45,19 +44,16 @@ echo
 echo "After connection is established in your local browser go to the address:"
 echo "http://localhost:${PORT}"
 
-exit 0   # exit program
-
 jupyter lab --no-browser --port $PORT
 
 
 
-OUTDIR = $PWD
-cp /home/jasperdj/analysis/B.E.13.B1950TRC5.ne30g16.ihesp24.sai2050.01/archive/atm/hist/B.E.13.B1950TRC5.ne30g16.ihesp24.sai2050.01.cam.h0.2060-??.nc $TMPDIR
-cp /home/jasperdj/create_tseries_dask.py $TMPDIR
-cd $TMPDIR
+#OUTDIR = $PWD
+#cp /home/jasperdj/analysis/B.E.13.B1950TRC5.ne30g16.ihesp24.sai2050.01/archive/atm/hist/B.E.13.B1950TRC5.ne30g16.ihesp24.sai2050.01.cam.h0.2060-??.nc $TMPDIR
+#cp /home/jasperdj/create_tseries_dask.py $TMPDIR
+#cd $TMPDIR
 
-python create_tseries_dask.py 2>$&1 > log.txt
+#python create_tseries_dask.py 2>$&1 > log.txt
 
-mv log.txt $OUTDIR
-mv dask-report.html $OUTDIR
-
+#mv log.txt $OUTDIR
+#mv dask-report.html $OUTDIR
