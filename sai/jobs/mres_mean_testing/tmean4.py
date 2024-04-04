1cfa5462057950080f607b8cb7b7ea55e7f67500
Jasper de Jong <j.dejong3@uu.nl>
2024-04-04 12:04:58 +0200

    run tests

--- sai/jobs/mres_mean_testing/tmean4.py
+++ sai/jobs/mres_mean_testing/tmean4.py
@@ -8,10 +8,12 @@ files = sorted(sys.argv[1:-1])
 outfile = sys.argv[-1]
 
 def main():    
+    with xr.open_dataset(files[0]) as ds:
+        print(ds)
     client = Client()
     print(client)
     with performance_report(filename='dask-report.html'):
-        with xr.open_mfdataset(files, chunks='auto', parallel=True, drop_variables=['date_written','time_written']) as ds:
+        with xr.open_mfdataset(files, chunks='auto', parallel=True, drop_variables=['time_bnds','date_written','time_written']) as ds:
             print(ds.chunks, flush=True)
             ds.mean('time').to_netcdf(outfile)
         print(f"created {outfile}")
