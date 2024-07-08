1d0251dae342ceb630dbf5404f2eba991ada359d
Jasper de Jong <j.dejong3@uu.nl>
2024-07-08 10:04:25 +0200

    created job

--- sai/scripts/temperaturegradients.py
+++ sai/scripts/temperaturegradients.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 # *_* coding: utf-8 *_*
 
-"""Calculate temperature gradients T0, T1 and T2 time series as in the GLENS paper"""
-
-infiles = '/Users/jasperdejong/Documents/PhD/Geo/sai-git/sai/data/deg5/mres_b.e10.B2000_CAM5.5deg.001.cam2.h0.????-??.nc'
-outfile = 'mres.sai2050.tempgradients.nc'
 
 def main():
+    # read script arguments 
+    parser = argparse.ArgumentParser(
+        description='Calculate temperature gradients T0, T1 and T2 time series as in GLENS')
+    parser.add_argument('infiles', nargs='+', help='input netCDF files')
+    parser.add_argument('outfile', help='output netCDF file')
+    args = parser.parse_args()
+    if os.path.exists(args.outfile):
+        raise ValueError(f'output file {args.outfile} already exists.')
+    
     # open dataset and set time to center of time_bnds
-    ds = xr.open_mfdataset(infiles, data_vars="minimal", coords="minimal", 
-                           join="exact", compat="override", chunks=None)
+    ds = xr.open_mfdataset(args.infiles, data_vars="minimal", coords="minimal", 
+        join="exact", compat="override", chunks=None, parallel=True)
     time = ('time', ds.time_bnds.mean('nbnd').data, ds.time.attrs)
     ds = ds.assign_coords({'ctime':time}).swap_dims({'time':'ctime'})
     ds = ds.drop_vars('time').rename({'ctime':'time'})
@@ -20,12 +25,16 @@ def main():
     T0 = gmean(ds.TREFHT).rename('T0')
     T1 = gmean(ds.TREFHT*sinlat).rename('T1')
     T2 = gmean(ds.TREFHT*(3*sinlat**2-1)/2).rename('T2')
-    xr.merge((T0,T1,T2)).groupby('time.year').mean('time').to_netcdf(outfile)
+    xr.merge((T0,T1,T2)).groupby('time.year').mean('time').to_netcdf(args.outfile)
     
 
 if __name__ == '__main__':
+    import os
+    import sys
+    import argparse
     import numpy as np
     import xarray as xr
     from dask.distributed import Client
     client = Client() # set up local cluster
+    print(client)
     main()
