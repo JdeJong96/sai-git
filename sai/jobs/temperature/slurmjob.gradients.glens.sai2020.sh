863aed6a7f93ba7ed7614ec362cd9798087dd868
Jasper <j.dejong3@uu.nl>
2024-07-09 22:26:44 -0400

    update

--- sai/jobs/temperature/slurmjob.gradients.glens.sai2020.sh
+++ sai/jobs/temperature/slurmjob.gradients.glens.sai2020.sh
@@ -5,13 +5,13 @@
 #SBATCH --time=00:15:00      	# wall clock time
 #SBATCH --nodes=1            	# number of nodes
 #SBATCH --partition=rome,genoa	# partition
-#SBATCH --job-name=mres50 	# job name
+#SBATCH --job-name=glens20 	# job name
 #SBATCH --account=uuce20038    	# account
 #SBATCH --ntasks=1           	# number of tasks
 #SBATCH --cpus-per-task=16   	# cpus per task
 
 echo "$(date +"%D %T"): Running $0"
-FILES=/projects/0/nwo2021025/archive/mres_b.e10.B2000_CAM5.f05_t12.001/atm/hist/mres_b.e10.B2000_CAM5.f05_t12.001.cam2.h0.????-??.nc
+FILES=/
 source $HOME/.bashrc
 conda activate geo
-python temperaturegradients.py $FILES data/temperaturegradients.mres.sai2050.nc
+python temperaturegradients.py $FILES data/temperaturegradients.glens.sai2020.nc
--- sai/jobs/windshear/snellius/windshear.py
+++ sai/jobs/windshear/snellius/windshear.py
@@ -1,57 +1,30 @@
-#!/usr/bin/env python3
-# *_* coding: utf-8 *_*
-
-"""Read CESM output data and calculate vertical wind shear
-
-Run with >> python windshear.py [-v,--verbose] *files outfile
-
-This script reads CESM output data, interpolates variables NAMES to
-pressure levels PLEVS. The wind shear is then calculated as the 
-difference between the horizontal winds at these levels. 
-
-The interpolation is performed linearly in ln(P) coordinates by applying
-logpressure_interp1d_gu() over every 1D vertical column in the data. If 
-the  pressure P is not present in the data, P will be calculated using
-FORMULA_TERMS, a mapping to relevant dataset variables to convert
-hybrid coordinates to pressure. 
-
-Parallel computing is supported by mapping the 1D vertical interpolation
-function to different chunks of the data arrays, controllable by the
-CHUNKS parameter. Mind that the vertical dimension is not allowed to be 
-chunked because the gufunc operates along this dimension (leave VDIM:-1)
-
-The vertical wind shear and other variables are stored in outfile.
-"""
-
 import os
 import sys
 import time
 import argparse
 import logging
-print(f'Python version: {sys.version}')
-print(f'Path: {sys.path}')
+#print(f'Python version: {sys.version}')
+#print(f'Path: {sys.path}')
 
 from numba import guvectorize
 import numpy as np
+from dask.distributed import Client
 import xarray as xr
 xr.set_options(keep_attrs=True)
 #xr.show_versions()
 
-# define constants
-YEAR_RANGE = range(2070,2073) # years to analyze
 PLEVS = (250, 850)  # pressure levels (hPa) for shear calculation
-PRES = 'P'    # air pressure 
-HYAM = 'hyam' # hybrid a coefficient (only used if P not present)
-HYBM = 'hybm' # hybrid b coefficient (see HYAM)
-PS = 'PS'     # surface pressure (see HYAM)
-P0 = 'P0'     # reference pressure hybrid coordinate (see HYAM)
-NAMES = []   # list of variables to interpolate/store
-U, V = 'U', 'V' # zonal and meridional wind
+
+# variable names (change values if needed)
+# VARS = {'U':'U','V':'V','P':'P','gw':'gw', 'time_bnds':'time_bnds','lsm':'lsm'} # pressure levels
+VARS = {'U':'U','V':'V','hyam':'hyam', 'hybm':'hybm','P0':'P0','PS':'PS',
+        'gw':'gw', 'time_bnds':'time_bnds','lsm':'LANDFRAC'} # hybrid model levels
+
 LABEL = 'upper' # ['upper','lower'], level where VSHEAR is defined
 VDIM = 'lev'  # vertical dimension (e.g. 'lev','plev','z','hybrid')
 CHUNKS = {'time':1,'ncol':'auto', VDIM:-1} # array chunk size for parallel computation
 NEWPRES = xr.DataArray(
-    data=100*np.array(sorted(PLEVS, reverse=True), dtype='float64'),
+    data=100*np.array(sorted(PLEVS), dtype='float64'),
     dims='plev',
     name='p',
     attrs={'standard_name':'air_pressure',
@@ -67,11 +40,11 @@ NEWPRES = xr.DataArray(
 def logpressure_interp1d_gu(f, p, pi, out):
     """interpolate field f(p) to pi in ln(p) coordinates"""
     i, imax, p0, f0 = 0, len(pi), p[0], f[0]
-    while pi[i]>p0 and i < imax:
+    while pi[i]<p0 and i < imax:
         out[i] = np.nan      
         i = i + 1 
     for p1,f1 in zip(p[1:], f[1:]):
-        while pi[i] >= p1 and i < imax:
+        while pi[i] <= p1 and i < imax:
             out[i] = (f1-f0)/np.log(p1/p0)*np.log(pi[i]/p0)+f0
             i = i + 1
         p0, f0 = p1, f1
@@ -83,12 +56,12 @@ def logpressure_interp1d_gu(f, p, pi, out):
 def xr_interpolate_pressure(ds):
     """wrapper for logpressure_interp1d_gu"""
     # calculate pressure if needed and interpolate
-    if PRES not in ds:
-        logging.info(f"calculating {PRES} from hybrid parameters")
-        pres = ds[HYAM] * ds[P0] + ds[HYBM] * ds[PS]   
+    if 'P' not in VARS:
+        logging.info(f"calculating 'P' from hybrid parameters")
+        pres = ds[VARS['hyam']] * ds[VARS['P0']] + ds[VARS['hybm']] * ds[VARS['PS']]
     else:
-        pres = ds[PRES]
-    ds = ds[NAMES].assign_coords({NEWPRES.name:NEWPRES})
+        pres = ds[VARS['P']]
+    ds = ds.assign_coords({NEWPRES.name:NEWPRES})
     logging.info(f"new coordinate: {ds[NEWPRES.name].coords}")
     logging.info("starting interpolation...")
     vdimvars = [v for v in ds.data_vars if hasattr(ds[v], VDIM)]
@@ -102,41 +75,14 @@ def xr_interpolate_pressure(ds):
         dask="parallelized",
         keep_attrs=True,
     ).transpose(*NEWPRES.dims,...)
-    
-
-def wind_shear(ds):
-    """Calculate windshear"""
-    USHEAR = ds[U].diff(NEWPRES.name, label=LABEL)
-    VSHEAR = ds[V].diff(NEWPRES.name, label=LABEL)
-    VWS = np.sqrt(USHEAR**2 + VSHEAR**2)
-    levstr = f'{max(PLEVS)}-{min(PLEVS)} hPa'
-    USHEAR.attrs.update({
-        'long_name':'eastward wind shear'+levstr,
-        'standard_name':'eastward_wind_shear',
-        'units':ds[U].units,
-    })
-    VSHEAR.attrs.update({
-        'long_name':'northward wind shear'+levstr,
-        'standard_name':'northward_wind_shear',
-        'units':ds[V].units,
-    })
-    VWS.attrs.update({
-        'long_name':'wind speed shear'+levstr,
-        'standard_name':'wind_speed_shear',
-        'units':ds[U].units,
-    })
-    ds['Ushear'] = USHEAR
-    ds['Vshear'] = VSHEAR
-    ds['VWS'] = VWS
     return ds
-
+    
 
 def check_globals(ds):
     """Check global variables defined in this file
     
     When faulty, quits program with helpful error messages
     """
-    global NAMES
     fatal = False
     if VDIM not in ds:
         logging.error(f"vertical dimension {VDIM} not in dataset")
@@ -148,14 +94,14 @@ def check_globals(ds):
         fatal = True
     else:
         logging.info(f"new pressure levels: {PLEVS} hPa")
-    if (PRES not in ds):
-        missing_terms = [t for t in [HYAM,HYBM,P0,PS] if t not in ds]
+    if ('P' not in VARS):
+        missing_terms = [VARS[t] for t in ['hyam','hybm','P0','PS'] if VARS[t] not in ds]
         if any(missing_terms):
             logging.error(f"{missing_terms} not in dataset")
             fatal = True
         else:
-            for term in [PS,P0]:
-                var = ds[term]
+            for v in ['PS','P0']:
+                var = ds[VARS[v]]
                 if not hasattr(var, 'units'):
                     logging.warning(f"assuming {var.name} has units 'Pa'")
                 elif var.units in ['Pa','pa']:
@@ -163,40 +109,19 @@ def check_globals(ds):
                 else:
                     logging.error(f"{var.name} units: {var.units}, expected Pa")
                     fatal = True
-            logging.info(f"using {HYAM,HYBM,P0,PS} to calculate '{PRES}'")
+            logging.info(f"using {[VARS[v] for v in ['hyam','hybm','P0','PS']]} to calculate pressure")
     else:
-        logging.info(f"using pressure '{PRES}'")
-    names = NAMES.copy() if len(NAMES) > 0 else list(ds.data_vars)
-    validnames = []
-    for name in names:
-        if name not in ds:
-            logging.warning(f"variable {name} not in dataset")
-        else:
-            validnames.append(name)
-    NAMES = validnames
-    if len(NAMES) == 0:
-        logging.error("no variables found in dataset")
-        fatal = True
-    else:
-        logging.info(f"variables to interpolate: {NAMES}")
+        logging.info(f"using pressure '{VARS['P']}'")
     if fatal:
         logging.critical(f"fatal errors occurred for dataset:\n{ds}")
         logging.critical(f"resolve errors first, aborting...")
         sys.exit(1)
-
-
-def write_file(ds, fname, year):
-    """add year to filename and save as netCDF"""
-    fnameparts = list(os.path.splitext(fname))
-    fnameparts.insert(1,f"{year:04}")
-    newfname = ''.join(fnameparts)
-    logging.info(f"writing to {os.path.abspath(fname)}")
-    ds.to_netcdf(newfname)
-    logging.info(f"SUCCES")
+    return
 
 
 def main():
     time0 = time.perf_counter()  # start timer
+    client = Client() # for parallel computing
 
     # parse command line arguments
     parser = argparse.ArgumentParser()
@@ -206,6 +131,8 @@ def main():
     parser.add_argument('-v', '--verbose', help='modify output verbosity', 
                         action='store_true')
     args = parser.parse_args()
+    if os.path.exists(args.outfile):
+        raise ValueError(f'file {args.outfile} already exists')
 
     # set up logger 
     logging.basicConfig(
@@ -219,31 +146,36 @@ def main():
         f"\n{os.linesep.join([os.path.basename(f) for f in args.files])}"
     )
     
-    # 
-    for year in YEAR_RANGE:
-        time1 = time.perf_counter()  # start timer
-        files_year = [f for f in args.files 
-                      if int(f.split('.')[-2][:4]) == year]
-        logging.info(f"opening {files_year} with chunks {CHUNKS}")
-        logging.info(
-            "opening:"
-            f"\n{os.linesep.join([os.path.basename(f) for f in files_year])}"
-            f"with chunks {CHUNKS}"
-        )
-        try:
-            datasets = [xr.open_dataset(f, chunks=CHUNKS) for f in files_year]
-        except Exception:
-            [ds.close() for ds in datasets]
-            raise
-        with xr.merge(datasets, join='exact', 
-                      combine_attrs='no_conflicts') as ds:
-            logging.info(f"...succes!")
-            check_globals(ds)
-            ds = wind_shear(ds) # calculate wind shear
-            write_file(ds, args.outfile, year)
-            time2 = time.perf_counter()
-            logging.info(f"processed year {year} in {time2-time1:.2f} seconds")
-    logging.info(f"total script time: {time2-time0:.2f} seconds")
+    # wind shear calculation
+    time1 = time.perf_counter()  # start timer
+    levstr = f'{max(PLEVS)}-{min(PLEVS)} hPa'
+    logging.info(f"opening [{args.files[0]} - {args.files[-1]}] with chunks {CHUNKS}")
+    with xr.open_mfdataset(args.files, data_vars="minimal", coords="minimal", 
+        join="exact", compat="override", chunks=CHUNKS) as ds:
+        time2 = time.perf_counter()
+        logging.info(f"...succes! opening took {time2-time1:.2f} seconds")
+        time = ('time', ds.time_bnds.mean('nbnd').data, ds.time.attrs)
+        ds = ds.assign_coords({'ctime':time}).swap_dims({'time':'ctime'})
+        ds = ds.drop_vars('time').rename({'ctime':'time'})
+        ds = ds[list(VARS.values())]
+        check_globals(ds)
+        hasplev = NEWPRES.dims[0] in ds.dims
+        if (not hasplev) or (hasplev and (not (ds[NEWPRES.dims[0]] == NEWPRES).all())):
+            ds = xr_interpolate_pressure(ds)
+        USHEAR = ds[VARS['U']].diff(NEWPRES.dims[0], label=LABEL).squeeze()
+        VSHEAR = ds[VARS['V']].diff(NEWPRES.dims[0], label=LABEL).squeeze()
+        ds['VWS'] = np.sqrt(USHEAR**2 + VSHEAR**2)
+        ds.VWS.attrs.update({
+            'long_name':'vertical wind shear '+levstr,
+            'standard_name':'wind_speed_shear',
+            'units':ds[VARS['U']].units,
+        })
+        ds.attrs.update({'history':
+            f'python windshear.py [{args.files[0]} - {args.files[-1]}] {args.outfile}'})
+        ds[['VWS',VARS['gw'],VARS['time_bnds'],VARS['lsm']]].to_netcdf(args.outfile)
+        time3 = time.perf_counter()
+        logging.info(f"processed all data in {time3-time1:.2f} seconds")
+    return
 
 
 if __name__ == '__main__':
